a0143972e752d7d87fddfde94d4a6d6a55e4fe25
SharipovsDev <85775884+sharipovme@users.noreply.github.com>
2021-06-20 00:46:37 +1300

    Solution

--- Warmup-1/26_max1020.js
+++ Warmup-1/26_max1020.js
@@ -1,6 +1,6 @@
 function max1020(a, b) {
   if (a >= 10 && a <= 20 && b >= 10 && b <= 20) return a > b ? a : b;
-  else if (a >= 10 && a <= 20) return a;
+  else if (a >= 10  && a <= 20) return a;
   else if (b >= 10 && b <= 20) return b;
   else return 0;
 }
